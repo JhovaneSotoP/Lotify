e736f694686b2597335031222d152bc8f8076f37
Jhovane <jhovanesoto@gmail.com>
2025-01-23 19:04:10 -0600

    Se comienza con la integracion de la interfaz grafica

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton,QDoubleSpinBox,QSpinBox
 from PyQt5 import uic
+from tableModule import lotteryTable
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uipath="Lotify/gui/mainView.ui"
+        uipath="gui/mainView.ui"
         uic.loadUi(uipath,self)
         self.load_gui()
     
@@ -18,7 +19,7 @@ class MainWindow(QMainWindow):
         self.stacked.setCurrentIndex(0)
 
         #Inicio
-        self.findChild(QPushButton,"btnIniciar").clicked.connect(self.nextIndexStacked)
+        self.findChild(QPushButton,"btnIniciar").clicked.connect(self.configuracionTabla)
         self.findChild(QPushButton,"btnSalir1").clicked.connect(self.exit)
 
         #Configuraxion de tablas
@@ -47,7 +48,39 @@ class MainWindow(QMainWindow):
         Termina el programa.
         """
         self.close()
+    def configuracionTabla(self):
+        self.nextIndexStacked()
 
+        self.item_altoTabla=self.findChild(QDoubleSpinBox,"altoTabla")
+        self.item_altoTabla.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.item_anchoTabla=self.findChild(QDoubleSpinBox,"anchoTabla")
+        self.item_anchoTabla.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.item_elementosLado=self.findChild(QSpinBox,"elementosLado")
+        self.item_elementosLado.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.item_bordeImpresion=self.findChild(QDoubleSpinBox,"bordeImpresion")
+        self.item_bordeImpresion.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.item_espaciadoCartas=self.findChild(QDoubleSpinBox,"espaciadoCartas")
+        self.item_espaciadoCartas.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.item_encabezadoAltura=self.findChild(QDoubleSpinBox,"encabezadoAltura")
+        self.item_encabezadoAltura.valueChanged.connect(self.actualizarComponentesTabla)
+
+        self.miniatura=lotteryTable()
+
+        self.actualizarComponentesTabla()
+
+    def actualizarComponentesTabla(self):
+        """
+        Actualiza todos los compoenentes de los parametros iniciales de las tablas.
+        """
+        size=(self.item_anchoTabla.value(),self.item_altoTabla.value())
+        self.miniatura.actualizarSize(size)
+        self.miniatura.actualizarElementosLado(self.item_elementosLado.value())
+        self.miniatura.imagen.show()
 
 
 app=QApplication(sys.argv)
