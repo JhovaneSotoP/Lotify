e422b55fb1e1e9b9ee6e60a9a77cbe0403a2d633
Jhovane <jhovanesoto@gmail.com>
2025-01-25 19:44:47 -0600

    Se hixo funcional la seccion de importar imagenes.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton,QDoubleSpinBox,QSpinBox,QFileDialog,QLabel,QColorDialog,QLineEdit, QFontComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton,QDoubleSpinBox,QSpinBox,QFileDialog,QLabel,QColorDialog,QLineEdit, QFontComboBox,QTableWidget,QTableWidgetItem
 from PyQt5 import uic
 from PyQt5.QtCore import QThread,pyqtSignal,Qt
 from PyQt5.QtGui import QPixmap
 from tableModule import lotteryTable
+import os
+import random
 
 loteria=lotteryTable()
 #HILOS
@@ -65,7 +67,7 @@ class MainWindow(QMainWindow):
         self.findChild(QPushButton,"btnSalir2").clicked.connect(self.exit)
 
         #Seleccionar imagenes
-        self.findChild(QPushButton,"btnContinuar2").clicked.connect(self.nextIndexStacked)
+        
         self.findChild(QPushButton,"btnSalir3").clicked.connect(self.exit)
 
         #Configuracion de cartas
@@ -125,7 +127,21 @@ class MainWindow(QMainWindow):
 
         self.item_labelTabla=self.findChild(QLabel,"previsualizacionTabla")
 
-        #HUI
+        #GUI importar imagenes
+        self.item_tablaImagenes=self.findChild(QTableWidget,"tablaImagenes")
+        self.item_tablaImagenes.model().rowsInserted.connect(self.tablaActualizada)
+        self.item_tablaImagenes.model().rowsRemoved.connect(self.tablaActualizada)
+
+        self.item_importarImagen=self.findChild(QPushButton,"importarImagen")
+        self.item_importarImagen.clicked.connect(self.importarImagenes)
+
+        self.item_eliminarImagen=self.findChild(QPushButton,"eliminarImagen")
+        self.item_eliminarImagen.clicked.connect(self.remove_row)
+
+        self.item_continuarImportacion=self.findChild(QPushButton,"btnContinuar2")
+        self.item_continuarImportacion.clicked.connect(self.continuarImportacion)
+        self.item_continuarImportacion.setEnabled(False)
+        
         
     def nextIndexStacked(self):
         """
@@ -142,12 +158,13 @@ class MainWindow(QMainWindow):
     def configuracionTabla(self):
         self.nextIndexStacked()
 
-        self.tabla=lotteryTable()
 
         self.hiloMiniaturaTabla=hiloMiniaturaTabla()
         self.hiloMiniaturaTabla.finished.connect(self.actualizarMiniaturaTabla)
 
         self.actualizarComponentesTabla()
+
+        self.dataBrutaCartas=[]
 
     def actualizarComponentesTabla(self):
         """
@@ -250,6 +267,88 @@ class MainWindow(QMainWindow):
         Cambia el color del fondo del boton enviado.
         """
         boton.setStyleSheet(f"background:{color};")
+    
+    def importarImagenes(self):
+        data=self.seleccionarImagenesPath()
+        for n in data:
+            count=self.item_tablaImagenes.rowCount()
+            self.item_tablaImagenes.insertRow(count)
+            temp=os.path.splitext(os.path.basename(n))[0]
+            temp=temp.split("_")
+
+            self.item_tablaImagenes.setItem(count,0,QTableWidgetItem(temp[0]))
+
+            if len(temp)>1:
+                try:
+                    datoPrueba=int(temp[1])
+                    dato2=temp[1]
+
+                except:
+                    dato2=""
+            else: 
+                dato2=""
+            
+            self.item_tablaImagenes.setItem(count,1,QTableWidgetItem(dato2))
+            self.item_tablaImagenes.setItem(count,2,QTableWidgetItem(n))
+        self.item_tablaImagenes.resizeColumnsToContents()
+    
+    def seleccionarImagenesPath(self):
+        """
+        Funcion que abre un dialogo para seleccionar imagenes (JPG y PNG) y retorna su path.
+
+        Return:
+        salida(list): lista de path que dirige a las imagenes seleccionadas.
+        """
+        salida=QFileDialog.getOpenFileNames(self,"Seleccionar imagen","","Imagenes (*.jpg *.png)")
+        if salida==([], ''):
+            return None
+        else:
+            return salida[0]
+    
+    def tablaActualizada(self):
+        if self.item_tablaImagenes.rowCount()>0:
+            self.item_continuarImportacion.setEnabled(True)
+        else:
+            self.item_continuarImportacion.setEnabled(False)
+    
+    def remove_row(self):
+        selected_rows = self.item_tablaImagenes.selectionModel().selectedRows()
+        for index in sorted(selected_rows, reverse=True):  # Eliminar de abajo hacia arriba
+            self.item_tablaImagenes.removeRow(index.row())
+    
+    def continuarImportacion(self):
+        num=[]
+        for n in range(self.item_tablaImagenes.rowCount()):
+            num.append(n)
+
+
+        for n in range(self.item_tablaImagenes.rowCount()):
+            dic={}
+
+            item=self.item_tablaImagenes.item(n,0)
+            data=item.text() if item else ""
+            dic.update({"titulo":data})
+
+            item=self.item_tablaImagenes.item(n,1)
+            data=item.text() if item else ""
+            if data=="":
+                numero=random.choice(num)
+                num.remove(numero)
+                data=str(numero)
+            dic.update({"numero":data})
+
+            item=self.item_tablaImagenes.item(n,2)
+            data=item.text() if item else ""
+
+            if os.path.exists(data):
+                dic.update({"path":data})
+            else:
+                print(data)
+                continue
+            
+            self.dataBrutaCartas.append(dic)
+        print(self.dataBrutaCartas)
+        self.nextIndexStacked()
 
 
 
